873b3205baa3f293d9c1bc7ce4a31020d726e793
unknown <henrique_barretto@outlook.com>
2020-08-22 15:20:02 -0300

    add: rota de delete

--- src/main/java/com/bandtec/continuada01Henrique/Autenticacao.java
+++ src/main/java/com/bandtec/continuada01Henrique/Autenticacao.java
@@ -1,22 +1,42 @@
 package com.bandtec.continuada01Henrique;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/autenticacao")
 public class Autenticacao {
+    String pattern = "dd-MM-yyyy hh-mm-ss";
     String usuario = "fulano";
     String senha = "123loko";
+    String dataLogin = "";
+    boolean auth= false;
 
     @PostMapping("/entrar")
-    public void logar(@RequestBody String usuario, @RequestBody String senha) {
-        if (usuario == this.usuario) {
+    public String logar(@RequestParam(value = "usuario") String usuario, @RequestParam(value = "senha") String senha) {
+
+        if (usuario.equals(this.usuario)) {
             if (senha.equals(this.senha)) {
+                this.auth = true;
+                dataLogin = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date());
+
                 System.out.println("Autenticado com sucesso!");
-                return;
+                return "Autenticado com sucesso!";
             }
         }
 
         System.out.println("Login ou senha incorretos");
+        return "Login ou senha incorretos";
     }
+
+    @DeleteMapping("")
+    public void deletar() {
+        this.auth = false;
+    }
+
 }
