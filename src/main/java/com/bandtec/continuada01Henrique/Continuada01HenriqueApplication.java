966caa22d4ac5a65fef809839ebc9c5d12cfdf2d
unknown <henrique_barretto@outlook.com>
2020-08-21 23:04:38 -0300

    setup

---
+++ src/main/java/com/bandtec/continuada01Henrique/Continuada01HenriqueApplication.java
@@ -0,0 +1,13 @@
+package com.bandtec.continuada01Henrique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Continuada01HenriqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Continuada01HenriqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bandtec/continuada01Henrique/Continuada01HenriqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandtec.continuada01Henrique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Continuada01HenriqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
