eb7081fd6260fa6cd5ce54cba4778fc4ee8e757a
unknown <henrique_barretto@outlook.com>
2020-08-22 16:41:19 -0300

    add: interface de autenticação

--- src/main/java/com/bandtec/continuada01Henrique/Autenticacao.java
+++ src/main/java/com/bandtec/continuada01Henrique/Autenticacao.java
@@ -7,40 +7,68 @@ import java.util.Date;
 
 @RestController
 @RequestMapping("/autenticacao")
-public class Autenticacao {
-    String usuario = "fulano";
-    String senha = "123loko";
+public class Autenticacao implements Autenticar {
+
+    // Suposta conta de admin
+    String adminUsuario = "admin";
+    String adminSenha = "admin";
+
+    // Suposta conta de cliente
+    String clientUsuario = "fulano";
+    String clientSenha = "123loko";
+
     String dataLogin = "";
-    boolean auth= false;
+    boolean auth = false;
+    boolean admin = false;
 
     @PostMapping("/entrar")
     public String logar(@RequestParam(value = "usuario") String usuario, @RequestParam(value = "senha") String senha) {
 
-        if (usuario.equals(this.usuario)) {
-            if (senha.equals(this.senha)) {
+        if (usuario.equals(this.adminUsuario)) {
+            if (senha.equals(this.adminSenha)) {
                 this.auth = true;
+                this.admin = true;
                 dataLogin = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date());
-
-                System.out.println("Autenticado com sucesso!");
                 return "Autenticado com sucesso!";
             }
         }
 
-        System.out.println("Login ou senha incorretos");
+        if (usuario.equals(this.clientUsuario)) {
+            if (senha.equals(this.clientSenha)) {
+                this.auth = true;
+                this.admin = false;
+                dataLogin = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date());
+                return "Autenticado com sucesso!";
+            }
+        }
         return "Login ou senha incorretos";
     }
 
     @DeleteMapping("")
     public void deletar() {
         this.auth = false;
+        this.admin = false;
     }
 
     @GetMapping("")
     public String pegarUsuarioAutenticado() {
-        if (this.auth == true) {
-            return "{ \"usuario\": \"" + this.usuario + "\", \"entrada\": \"" + this.dataLogin + "\"}";
+        if (isAuth()) {
+            if (isAdmin()) {
+                return "{ \"usuario\": \"" + this.adminUsuario + "\", \"entrada\": \"" + this.dataLogin + "\"}";
+            }
+            return "{ \"usuario\": \"" + this.clientUsuario + "\", \"entrada\": \"" + this.dataLogin + "\"}";
         } else {
             return "Usuário não autenticado";
         }
+    }
+
+    @Override
+    public boolean isAuth() {
+        return this.auth;
+    }
+
+    @Override
+    public boolean isAdmin() {
+        return this.admin;
     }
 }
---
+++ src/main/java/com/bandtec/continuada01Henrique/Autenticar.java
@@ -0,0 +1,8 @@
+package com.bandtec.continuada01Henrique;
+
+public interface Autenticar {
+
+    public boolean isAuth();
+
+    public boolean isAdmin();
+}
